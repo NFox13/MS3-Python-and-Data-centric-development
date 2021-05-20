bcb5ab936dbe96e2cee0b7e22e412f26dfbc83de
NFox13 <n_fox2000@yahoo.com>
2021-05-20 09:43:34 +0000

    Added functionality to the login page. Added style to both login and registration page and an aid for the username/password requirements.

--- app.py
+++ app.py
@@ -50,6 +50,31 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # check input password matches hashed password
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome back, {}".format(request.form.get("username")))
+            else:
+                # invalid password
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username is not in db
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
 @app.route("/get_recipes")
 def get_recipes():
     recipes = mongo.db.recipes.find()
