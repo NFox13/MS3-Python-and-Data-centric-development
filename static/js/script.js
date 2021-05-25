7cd2a045ec316f3ddc84b28e6b87c08519631491
NFox13 <n_fox2000@yahoo.com>
2021-05-25 09:57:55 +0000

    Added addZero function mins and secs.

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,31 @@
 //DOM Elements
 const time = document.getElementById("time"),
     greeting = document.getElementById("greeting");
+
+//Show Time
+function showTime() {
+    let today = new Date(),
+        hour = today.getHours(),
+        min = today.getMinutes(),
+        sec = today.getSeconds();
+
+    //Set AM or PM
+    const ampm = hour >= 12 ? "pm" : "am";
+
+    //12hr Format
+    hour = hour % 12 || 12;
+
+    //Output Time
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+
+    setTimeout(showTime, 1000);
+}
+
+//Add Zero
+function addZero(n) {
+    return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+
+//Run
+showTime();
     
