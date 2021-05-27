f7a562c6bff25fbf3b1a657209c47174d7b479f4
NFox13 <n_fox2000@yahoo.com>
2021-05-27 06:49:11 +0000

    Fixed app route to correct html name.

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ def get_products():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('return_404.html'), 404
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
